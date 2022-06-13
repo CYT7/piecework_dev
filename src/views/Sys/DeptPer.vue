c554b0a852e7ac2383bfb082c25c3054c9be6aa3
CYT7 <1490930847@qq.com>
2022-06-13 17:01:48 +0800

    系数模块基本完成了，剩下下载

--- src/views/Sys/DeptPer.vue
+++ src/views/Sys/DeptPer.vue
@@ -3,7 +3,9 @@
     <!--工具栏-->
     <div class="toolbar" style="float:left;padding-top:10px;padding-left:15px;">
       <el-form :inline="true" :model="filters" :size="size">
-        <el-form-item><el-input v-model="filters.name" placeholder="职工名" /></el-form-item>
+        <el-form-item>
+          <el-input v-model="filters.name" placeholder="职工名" />
+        </el-form-item>
         <el-form-item>
           <kt-button icon="fa fa-search" :label="$t('action.search')" perms="sys:DeptPer:view" type="primary" @click="findPage()"/>
         </el-form-item>
@@ -22,14 +24,13 @@
         </el-form-item>
         <el-form-item>
           <el-tooltip content="刷新" x-placement="top"><kt-button perms="sys:DeptPer:view" icon="fa fa-refresh" @click="findPage(null)"/></el-tooltip>
-          <el-tooltip content="导出" placement="top"><kt-button perms="sys:DeptPer:download" icon="fa fa-file-excel-o" @click="exportExcelFile"></kt-button></el-tooltip>
         </el-form-item>
       </el-form>
     </div>
     <div>
       <!--表格内容栏-->
-      <el-table :data="pageResult" v-if="pageResult[0]!= null" stripe size="mini" style="width: 100%;" v-loading="loading" element-loading-text="$t('action.loading')"
-                @selection-change="selectionChange">
+      <el-table :data="pageResult" v-if="pageResult[0]!= null" stripe size="mini" style="width: 100%;"
+                v-loading="loading" element-loading-text="$t('action.loading')" @selection-change="selectionChange">
         <el-table-column type="selection" width="40"/>
         <el-table-column type="expand" width="40">
           <template slot-scope="props">
@@ -48,15 +49,9 @@
         <el-table-column sortable prop="empNo" label="职工号" header-align="center" align="center" min-width="60%"/>
         <el-table-column sortable prop="empName" label="姓名" header-align="center" align="center" min-width="60%"/>
         <el-table-column sortable prop="month" label="月份" header-align="center" align="center" :formatter="dateFormat" min-width="60%"/>
-        <el-table-column sortable prop="status" label="状态" header-align="center" align="center" min-width="60%">
-          <template slot-scope="scope">
-            <el-tag v-if="scope.row.status === 0" size="small">已确认</el-tag>
-            <el-tag v-else-if="scope.row.status === 1" size="small">可编辑</el-tag>
-          </template>
-        </el-table-column>
         <el-table-column label="分数" header-align="center" align="center" min-width="60%">
           <el-table-column v-for="(item,i) in pageResult[0].scoreList" :key="i"
-                           :label="scoreFormat(item.points)"
+                           :label="scoreFormat(item.type)"
                            header-align="center" align="center" min-width="80%">
             <template slot-scope="scope">
               <span>{{scope.row.scoreList[i]?scope.row.scoreList[i].score:''}}</span>
@@ -65,6 +60,12 @@
         </el-table-column>
         <el-table-column sortable prop="score" label="总分数" header-align="center" align="center" min-width="50%"/>
         <el-table-column sortable prop="updateBy" label="更新人" header-align="center" align="center" min-width="60%"/>
+        <el-table-column sortable prop="status" label="状态" header-align="center" align="center" min-width="60%">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status === 0" size="small">确认</el-tag>
+            <el-tag v-else-if="scope.row.status === 1" size="small">未编辑</el-tag>
+          </template>
+        </el-table-column>
         <el-table-column header-align="center" align="center" :label="$t('action.operation')" min-width="100%">
           <template slot-scope="scope" v-if="scope.row.status === 1">
             <kt-button icon="fa fa-edit" :label="$t('action.edit')" perms="sys:DeptPer:edit" @click="handleEdit(scope.row)"/>
@@ -125,7 +126,7 @@
           </el-select>
         </el-form-item>
         <el-form-item label="系数方案" prop="coefficientSid">
-          <el-select style="width: 100%" placeholder="选择系数方案" value-key="id" v-model="dataForm.coefficientSid">
+          <el-select style="width: 100%" placeholder="选择系数方案" value-key="id" v-model="dataForm.coefficientScheme">
             <el-option v-for="item in coeSchemeData" :key="item.id" :label="item.title" :value="item.id" @click.native="CoeSchemeCurrentChange(item)"></el-option>
           </el-select>
         </el-form-item>
@@ -276,6 +277,7 @@ export default {
         empNo: '',
         empName:'',
         month:'',
+        coefficientScheme:'',
         coefficientList: {}
       }
     },
@@ -447,9 +449,10 @@ export default {
     },
     empCurrentChangeHandle: function (data){
       this.dataForm.empNo = data.empNo
+      this.dataForm.empName = data.name
     },
     CoeSchemeCurrentChange:function (data){
-      this.dataForm.coefficientSid = data.id
+      this.dataForm.coefficientScheme = data.id
       this.findCoeList(data.id)
     },
     //上传鉴定
